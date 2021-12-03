10db0d6354b6faa348d9b3bfbfd385cb3f2e10c6
OliveBounty <oliveodobounty@gmail.com>
2021-12-03 11:09:21 -0500

    feat: media_path fix

--- src/app/constants/config.ts
+++ src/app/constants/config.ts
@@ -9,7 +9,7 @@ Purpose : This is the file which contain all global constants used in the applic
 export const config = {
     site_name: "Cryptotrades",
     // media_path: "https://exampleapi.com",
-    media_path: "https://pulse.io",
+    media_path: "https://pulseart.io",
     base_url: "https://pulseart.io",
     // base_url: "http://localhost:4200",
     google_client_id:'116318305608-1dgsvgdiss1fc9h8s7po0as10sjbhvnm.apps.googleusercontent.com',
